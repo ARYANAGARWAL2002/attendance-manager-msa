27e049a4a730add2dcd4897e9d419ba206bad7ae
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-11-02 11:00:37 +0530

    Refactor: Simplify CORS handling in SecurityConfig and remove unnecessary HttpMethod import

--- api-gateway/src/main/java/com/proj/api_gateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/proj/api_gateway/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.proj.api_gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+// We no longer need HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -14,16 +14,20 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
+                // ADD THIS LINE: This tells Spring Security to use the
+                // CORS configuration bean provided by Spring Cloud Gateway
+                // (which is loaded from your api-gateway.properties file)
+                .cors(cors -> {
+                })
+
                 // Disable CSRF (Cross-Site Request Forgery)
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
 
                 // Configure authorization rules
                 .authorizeExchange(exchanges -> exchanges
-                        // Allow all OPTIONS requests (used for CORS preflight)
-                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
-                        // Allow all other requests
-                        .anyExchange().permitAll()
-                );
+                        // We no longer need to permit OPTIONS manually,
+                        // the .cors() handler above will take care of it.
+                        .anyExchange().permitAll());
 
         return http.build();
     }
